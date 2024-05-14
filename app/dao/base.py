4ebcee5037e50c326fc7480776df8827e5bcfa56
Kanesha Urtots <kaneshau@et-e1.kzn.21-school.ru>
2024-05-14 22:44:31 +0300

    authentification added

--- app/dao/base.py
+++ app/dao/base.py
@@ -1,5 +1,5 @@
 from db import async_session_maker
-from sqlalchemy import select
+from sqlalchemy import insert, select
 
 
 class BaseDAO:
@@ -13,7 +13,7 @@ class BaseDAO:
             return result.scalar_one_or_none()
         
     @classmethod
-    async def find_one_or_none(cls, filter_by: dict):
+    async def find_one_or_none(cls, **filter_by):
         async with async_session_maker() as session:
             query = select(cls.model).filter_by(**filter_by)
             result = await session.execute(query)
@@ -25,3 +25,10 @@ class BaseDAO:
             query = select(cls.model).filter_by(**filter_by)
             result = await session.execute(query)
             return result.mappings().all()
+        
+    @classmethod
+    async def add(cls, **data):
+        async with async_session_maker() as session:
+            query = insert(cls.model).values(**data)
+            await session.execute(query)
+            await session.commit()
